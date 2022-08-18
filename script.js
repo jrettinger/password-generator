e5a19a98c273a51363a370880ab496c98c569063
unknown <jason.rettinger@gmail.com>
2022-08-17 22:43:16 -0400

    removed alert

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Assignment Code
-// Query Selectors
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
 
 // Variables for the letters, numbers & special characters
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -11,42 +9,40 @@ var specialCharacters = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 var totalChars = "lowerCase+upperCase+numbers+specialCharacters";
 var passwordLength = "";
 
+// Write password to the password input
+  function writePassword() {
+    var password = generatePassword();
+
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Alert user about series of questions determining password
-function generatePassword() {
-  alert("Welcome to the Password Generator. You will be asked a series of questions to help generate a random password.")
-}
 // Password Criteria
-var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
-var lowerCase = confirm("Do you want your password to contain lowercase letters?");
-var upperCase = confirm("Do you want your password to contain uppercase letters?");
-var numbers = confirm("Do you want your password to contain numbers?");
-var specialCharacters = confirm("Do you want your password to contain special characters?");
-var passwordLengthUser = prompt("How many characters would you like the password to be? Password MUST be between 8-128 characters.")
+var passwordLength = prompt("How many characters would you like the password to be? Password MUST be between 8-128 characters.");
 if (passwordLength < 8 || passwordLength > 128) {
   alert("Password length must be between 8-128 characters");
   determineLength();
 }else if (passwordLength>128){
   alert("Password length must be between 8-128 characters");
   determineLength();
+var lowerCase = confirm("Do you want your password to contain lowercase letters?");
+var upperCase = confirm("Do you want your password to contain uppercase letters?");
+var numbers = confirm("Do you want your password to contain numbers?");
+var specialCharacters = confirm("Do you want your password to contain special characters?");
   
-  return "Generated password";
+
 }
 function writePassword() {
   
   
   
-  // Write password to the password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
+
+ 
   // Write password to the #password input
 function generatePassword () {
   var totalChars = ""
