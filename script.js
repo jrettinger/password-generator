ba86716644f44e6df96afbeec01ecad00e1440c5
unknown <jason.rettinger@gmail.com>
2022-08-17 22:01:45 -0400

    added variables & prompts

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // Assignment Code
+// Query Selectors
 var generateBtn = document.querySelector("#generate");
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
-var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordText = document.querySelector("#password");
+
+// Variables for the letters, numbers & special characters
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+var totalChars = "lowerCase+upperCase+numbers+specialCharacters";
+var passwordLength = "";
 
 // Write password to the #password input
+function generatePassword () {
+var totalChars = ""
+var password = ""
+var passwordLength = document.getElementById("number").innerHTML;
+passwordText.value = password;
+// Alert user about series of questions determining password
+function generatePassword() {
+  alert("Welcome to the Password Generator. You will be asked a series of questions to help generate a random password.")
+}
+// User prompt to select the amount of characters for their password 
+var passwordLengthUser = prompt("How many characters would you like the password to be? Password MUST be between 8-128 characters.")
+if (passwordLength<8){
+  alert("Password length must be between 8-128 characters");
+  determineLength();
+}else if (passwordLength>128){
+  alert("Password length must be between 8-128 characters");
+  determineLength();
+
+  return "Generated password";
+}
 function writePassword() {
 
-<p id="number">91</p>
-<p id="demo"></p>
+
 
 <script>
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var passwordLength = document.getElementById("number").innerHTML;
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
-var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
-var totalChars = lowerCase+uppercaseChar+numberChar+specialChar;
-var password = "";
+  // Password Criteria
+    var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+    var lowerCase = confirm("Do you want your password to contain lowercase letters?");
+    var upperCase = confirm("Do you want your password to contain uppercase letters?");
+    var numbers = confirm("Do you want your password to contain numbers?");
+    var specialCharacters = confirm("Do you want your password to contain special characters?");
+
+
 
 if(passwordLength >= 8 && passwordLength <= 128){
 for(var i = 0; i < passwordLength; i++){
@@ -27,13 +53,14 @@ password += totalChars[i];
 }
 
 
+
 document.getElementById("demo").innerHTML = "password is " + password;
 </script>
 
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
-  passwordText.value = password;
+  
 
 }
 
